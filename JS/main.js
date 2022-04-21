5eb38d6efe254c385ae7a62fe13272d6f3ddd1b2
VnHoe <91834973+VnHoe@users.noreply.github.com>
2022-04-22 00:52:55 +0700

    make slider

--- JS/main.js
+++ JS/main.js
@@ -14,5 +14,34 @@ btn_Play.onclick = () => {
         main_page.style.display = 'block'
         background__heart.style.display = 'block'
         audioBox.play();
-    }, 4500);
+    }, 0);
 }
+
+window.addEventListener("load", function () {
+    const silder = document.querySelector('.silder');
+    const silderMain = document.querySelector('.slider-main');
+    const silderItems = document.querySelectorAll('.slider-item');
+    const silderItem_Width = silderItems[0].offsetWidth;
+    const sliderLength = silderItems.length;
+    const btnNext = document.querySelector('.slider-next');
+    const btnPrev = document.querySelector('.slider-pre');
+    let positionX = 0;
+    console.log(silderItem_Width);
+
+    btnNext.addEventListener("click", function () {
+        HandleChangeSlide(1)
+    })
+
+    btnPrev.addEventListener("click", function () {
+        HandleChangeSlide(-1)
+    })
+
+    function HandleChangeSlide(direction) { /* direction(chiều hướng) */
+        if (direction === 1) {
+            positionX = positionX - silderItem_Width ;
+            console.log(positionX);
+        } else if (direction === -1) {
+            positionX += silderItem_Width ;
+        }
+    }
+})
