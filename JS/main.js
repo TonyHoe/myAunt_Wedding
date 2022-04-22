03be2458715e94d9a33d08aac8591dd1ae0b82e4
VnHoe <91834973+VnHoe@users.noreply.github.com>
2022-04-22 15:43:41 +0700

    develop slider

--- JS/main.js
+++ JS/main.js
@@ -1,47 +1,67 @@
-var btn_Play = document.querySelector('#modal-play .ic-play');
-var main_page = document.getElementById('main_page');
-var background__heart = document.getElementById('back-ground__heart');
-
-btn_Play.onclick = () => {
-    var spinner = document.getElementById('spinner');
-    var beginPage = document.getElementById('begin');
-    var audioBox = document.querySelector('#audio-container .audio-content');
-
-    btn_Play.style.display = 'none';
-    spinner.style.display = 'block';
-    setTimeout(() => {
-        beginPage.style.display = 'none'
-        main_page.style.display = 'block'
-        background__heart.style.display = 'block'
-        audioBox.play();
-    }, 0);
-}
-
 window.addEventListener("load", function () {
-    const silder = document.querySelector('.silder');
-    const silderMain = document.querySelector('.slider-main');
-    const silderItems = document.querySelectorAll('.slider-item');
-    const silderItem_Width = silderItems[0].offsetWidth;
-    const sliderLength = silderItems.length;
-    const btnNext = document.querySelector('.slider-next');
-    const btnPrev = document.querySelector('.slider-pre');
-    let positionX = 0;
-    console.log(silderItem_Width);
+    var btn_Play = document.querySelector('#modal-play .ic-play');
+    var main_page = document.getElementById('main_page');
+    var background__heart = document.getElementById('back-ground__heart');
 
-    btnNext.addEventListener("click", function () {
-        HandleChangeSlide(1)
-    })
+    btn_Play.onclick = () => {
+        var spinner = document.getElementById('spinner');
+        var beginPage = document.getElementById('begin');
+        var audioBox = document.querySelector('#audio-container .audio-content');
+        //@ SLIDER
+        var slider = document.getElementById('#slider');
+        var sliderMain = document.querySelector('.slider-main');
+        var sliderItems = document.querySelectorAll('.slider_item');
+        var prevBtn = document.querySelector('.slider-prev');
+        var nextBtn = document.querySelector('.slider-next');
+        var slidersLength = sliderItems.length;
+        let positionX = 0;
+        let index = 0;
 
-    btnPrev.addEventListener("click", function () {
-        HandleChangeSlide(-1)
-    })
+        // Hiện loading
+        btn_Play.style.display = 'none';
+        spinner.style.display = 'block';
 
-    function HandleChangeSlide(direction) { /* direction(chiều hướng) */
-        if (direction === 1) {
-            positionX = positionX - silderItem_Width ;
-            console.log(positionX);
-        } else if (direction === -1) {
-            positionX += silderItem_Width ;
-        }
+        // thời gian hiện layout
+        setTimeout(() => {
+            beginPage.style.display = 'none'
+            main_page.style.display = 'block'
+            background__heart.style.display = 'block'
+            audioBox.play();
+
+            // Slider
+            var sliderItemWidth = sliderItems[0].offsetWidth;
+            nextBtn.addEventListener("click", function () {
+                HandleChangeSlide(1);
+                positionX -= sliderItemWidth;
+                index++;
+                sliderMain.style = `transform: translateX(${positionX}px)`
+                if( index >= slidersLength) {
+                    positionX = 0;
+                    sliderMain.style = `transform: translateX(${positionX}px)`
+                    index = 0;
+                }
+            });
+
+            prevBtn.addEventListener("click", function () {
+                HandleChangeSlide(-1);
+                positionX += sliderItemWidth;
+                index--;
+                sliderMain.style = `transform: translateX(${positionX}px)`
+                if( index <= 0) {
+                    positionX = 0;
+                    sliderMain.style = `transform: translateX(${positionX}px)`
+                    index = 0;
+                }
+            });
+
+            function HandleChangeSlide(direction) {
+                if (direction === 1) {
+
+                } else if (direction === -1) {
+
+                }
+            }
+        }, 0);
     }
+
 })
