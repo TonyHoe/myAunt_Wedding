f2ea7f6375f4c1177988031bdf14df80445270d1
VnHoe <91834973+VnHoe@users.noreply.github.com>
2022-04-22 23:24:49 +0700

    develop slider and background

--- JS/main.js
+++ JS/main.js
@@ -7,7 +7,7 @@ window.addEventListener("load", function () {
         var spinner = document.getElementById('spinner');
         var beginPage = document.getElementById('begin');
         var audioBox = document.querySelector('#audio-container .audio-content');
-        //@ SLIDER
+        //_ SLIDER
         var slider = document.getElementById('#slider');
         var sliderMain = document.querySelector('.slider-main');
         var sliderItems = document.querySelectorAll('.slider_item');
@@ -32,36 +32,38 @@ window.addEventListener("load", function () {
             var sliderItemWidth = sliderItems[0].offsetWidth;
             nextBtn.addEventListener("click", function () {
                 HandleChangeSlide(1);
-                positionX -= sliderItemWidth;
-                index++;
-                sliderMain.style = `transform: translateX(${positionX}px)`
-                if( index >= slidersLength) {
-                    positionX = 0;
-                    sliderMain.style = `transform: translateX(${positionX}px)`
-                    index = 0;
-                }
             });
 
             prevBtn.addEventListener("click", function () {
                 HandleChangeSlide(-1);
-                positionX += sliderItemWidth;
-                index--;
-                sliderMain.style = `transform: translateX(${positionX}px)`
-                if( index <= 0) {
-                    positionX = 0;
-                    sliderMain.style = `transform: translateX(${positionX}px)`
-                    index = 0;
-                }
             });
 
             function HandleChangeSlide(direction) {
                 if (direction === 1) {
-
+                    positionX -= sliderItemWidth;
+                    index++;
+                    sliderMain.style = `transform: translateX(${positionX}px)`
+                    if (index >= slidersLength) {
+                        positionX = 0;
+                        sliderMain.style = `transform: translateX(${positionX}px)`
+                        index = 0;
+                    }
                 } else if (direction === -1) {
-
+                    positionX += sliderItemWidth;
+                    index--;
+                    sliderMain.style = `transform: translateX(${positionX}px)`
+                    if (index <= 0) {
+                        positionX = 0;
+                        sliderMain.style = `transform: translateX(${positionX}px)`
+                        index = 0;
+                    }
                 }
             }
+            //_ auto change slide
+            setInterval(function () {
+                HandleChangeSlide(1);
+            }, 3000);
+
         }, 0);
     }
-
 })
